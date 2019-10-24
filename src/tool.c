ef6164ae404348ad0243eb74af899694b332ac73
Minjun Kim <mjaything@gmail.com>
2019-10-24 08:56:53 +0200

    utils_2.c done

--- src/tool.c
+++ src/tool.c
@@ -1,77 +1,9 @@
 #include "fdf.h"
 #include "../libft/libft.h"
 #include "../libft/get_next_line.h"
-
-static int  is_digit_base(char c, int base)
-{
-    const char  *digits = "0123456789ABCDEF";
-    int         i;
-
-    i = 0;
-    while (i < base)
-    {
-        if (digits[i] == ft_toupper(c))
-            return (i);
-        i++;
-    }
-    return (-1);
-}
-
-static t_bool has_prefix(const char *str, int base)
-{
-    size_t  i;
-
-    i = 0;
-    if (base == 2 || base == 8 || base == 16)
-    {
-        if (str[i++] != '0')
-            return (false);
-        if ((base == 2) && (str[i] == 'B' || str[i] == 'b'))
-            return (true);
-        if ((base == 16) && (str[i] == 'X' || str[i] == 'x'))
-            return (true);
-        if (base == 8)
-            return (true);
-    }
-    return (false);
-}
-
-int is_space(int c)
-{
-    return (c == ' ' ||
-            c == '\t' ||
-            c == '\n' ||
-            c == '\v' ||
-            c == '\f' ||
-            c == '\r');
-}
-
-t_bool is_number(char *str, int base)
-{
-    size_t  i;
-    size_t  digits;
-    t_bool  result;
-
-    i = 0;
-    digits = 0;
-    while (is_space(str[i]))
-        i++;
-    if (base != 10 && !has_prefix(&str[i], base))
-        return (false);
-    if ((base == 2) || (base == 16))
-        i += 2;
-    else if (base == 8)
-        i++;
-    else if ((base == 10) && (str[i] == '-' || str[i] == '+'))
-        i++;
-    while (is_digit_base(str[i++], base) >= 0)
-        digits++;
-    if (!str[i] && digits)
-        true;
-    else
-        false;
-    return (result);
-}
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int     atoi_base(const char *str, int base)
 {
@@ -124,7 +56,6 @@ t_point new_point(int x, int y, t_map *map)
         p.color = map->color_arr[i];
     return (p);
 }
-
 
 void	terminate(char *str)
 {
